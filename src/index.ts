03ff2a0c2c49fd8a9c331a5e578a34382d7c1132
Natalia Braz <naabraz@gmail.com>
2023-08-31 21:31:10 -0300

    feat: create login route

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import router from './routers'
 
 const PORT = process.env.PORT || 4000
 const HOSTNAME = process.env.HOSTNAME || 'http://localhost'
@@ -6,5 +7,7 @@ const HOSTNAME = process.env.HOSTNAME || 'http://localhost'
 const app = express()
 
 app.get('/', (_, res) => res.send('auth-service is healthy!'))
+
+app.use('/', router)
 
 app.listen(PORT, () => console.log(`Server up: ${HOSTNAME}:${PORT} 🚀`))
---
+++ src/routers/login.ts
@@ -0,0 +1,9 @@
+import { Router } from 'express'
+
+const router = Router()
+
+router.post('/login', (_, res) => {
+  res.status(200).send({ logged: true });
+})
+
+export default router;
