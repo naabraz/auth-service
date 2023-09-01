5412968566a21c7c44194a5d9b98b6d3e783a638
Natalia Braz <naabraz@gmail.com>
2023-08-31 21:14:51 -0300

    feat: add root endpoint

--- src/index.ts
+++ src/index.ts
@@ -5,4 +5,6 @@ const HOSTNAME = process.env.HOSTNAME || 'http://localhost'
 
 const app = express()
 
+app.get('/', (_, res) => res.send('auth-service is healthy!'))
+
 app.listen(PORT, () => console.log(`Server up: ${HOSTNAME}:${PORT} 🚀`))
